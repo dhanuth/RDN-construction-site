ff3be661481f789d407b1f97c034d125144886c9
dhanuth <dhanuthfernando@gmail.com>
2025-07-06 15:51:27 -0400

    Add files via upload
    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+export default {content: [
+  './index.html',
+  './src/**/*.{js,ts,jsx,tsx}'
+],
+  theme: {
+    extend: {
+      colors: {
+        'navy': {
+          700: '#1e3a8a',
+          800: '#172554',
+          900: '#0c1b44',
+        },
+        'orange': {
+          500: '#f97316',
+          600: '#ea580c',
+        },
+      },
+    },
+  },
+}
